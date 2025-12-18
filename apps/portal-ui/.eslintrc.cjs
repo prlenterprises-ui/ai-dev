b81126037f2162e123fb5aaf06118d198a22e09a
prlenterprises-ui <prl.enterprises@gmail.com>
2025-12-18 20:05:39 +0000

    ci: remove plugin:react-refresh/recommended from extends to avoid incompatible plugin config in CI

--- apps/portal-ui/.eslintrc.cjs
+++ apps/portal-ui/.eslintrc.cjs
@@ -8,7 +8,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react-refresh/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
